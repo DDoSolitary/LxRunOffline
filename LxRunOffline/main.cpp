2f631645c1a49f462e4a5d33b356e9309488088f
DDoSolitary <DDoSolitary@gmail.com>
2018-09-06 23:07:06 +0800

    Provide shorthands for commands.

--- LxRunOffline/main.cpp
+++ LxRunOffline/main.cpp
@@ -32,16 +32,16 @@ int wmain(int argc, wchar_t **argv) {
 		} else if (!wcscmp(argv[1], L"version")) {
 			std::wcout << L"LxRunOffline " << LXRUNOFFLINE_VERSION << std::endl;
 #endif
-		} else if (!wcscmp(argv[1], L"list")) {
+		} else if (!wcscmp(argv[1], L"l") || !wcscmp(argv[1], L"list")) {
 			for (crwstr s : list_distros()) {
 				std::wcout << s << std::endl;
 			}
-		} else if (!wcscmp(argv[1], L"get-default")) {
+		} else if (!wcscmp(argv[1], L"gd") || !wcscmp(argv[1], L"get-default")) {
 			std::wcout << get_default_distro() << std::endl;
-		} else if (!wcscmp(argv[1], L"set-default")) {
+		} else if (!wcscmp(argv[1], L"sd") || !wcscmp(argv[1], L"set-default")) {
 			parse_args();
 			set_default_distro(name);
-		} else if (!wcscmp(argv[1], L"install")) {
+		} else if (!wcscmp(argv[1], L"i") || !wcscmp(argv[1], L"install")) {
 			wstr dir, file, root, conf_path;
 			bool shortcut;
 			desc.add_options()
@@ -61,12 +61,12 @@ int wmain(int argc, wchar_t **argv) {
 				if (FAILED(hr)) throw error_hresult(err_create_shortcut, {}, hr);
 			}, &CoTaskMemFree);
 			if (shortcut) create_shortcut(name, dp.val + (L'\\' + name + L".lnk"), L"");
-		} else if (!wcscmp(argv[1], L"uninstall")) {
+		} else if (!wcscmp(argv[1], L"ui") || !wcscmp(argv[1], L"uninstall")) {
 			parse_args();
 			auto dir = get_distro_dir(name);
 			unregister_distro(name);
 			delete_directory(dir);
-		} else if (!wcscmp(argv[1], L"register")) {
+		} else if (!wcscmp(argv[1], L"rg") || !wcscmp(argv[1], L"register")) {
 			wstr dir, conf_path;
 			desc.add_options()
 				(",d", po::wvalue<wstr>(&dir)->required(), "The directory containing the distribution.")
@@ -76,16 +76,16 @@ int wmain(int argc, wchar_t **argv) {
 			if (!conf_path.empty()) conf.load_file(conf_path);
 			register_distro(name, dir);
 			conf.configure_distro(name, config_all);
-		} else if (!wcscmp(argv[1], L"unregister")) {
+		} else if (!wcscmp(argv[1], L"ur") || !wcscmp(argv[1], L"unregister")) {
 			parse_args();
 			unregister_distro(name);
-		} else if (!wcscmp(argv[1], L"move")) {
+		} else if (!wcscmp(argv[1], L"m") || !wcscmp(argv[1], L"move")) {
 			wstr dir;
 			desc.add_options()(",d", po::wvalue<wstr>(&dir)->required(), "The directory to move the distribution to.");
 			parse_args();
 			move_directory(get_distro_dir(name), dir);
 			set_distro_dir(name, dir);
-		} else if (!wcscmp(argv[1], L"duplicate")) {
+		} else if (!wcscmp(argv[1], L"d") || !wcscmp(argv[1], L"duplicate")) {
 			wstr new_name, dir, conf_path;
 			desc.add_options()
 				(",d", po::wvalue<wstr>(&dir)->required(), "The directory to copy the distribution to.")
@@ -98,7 +98,7 @@ int wmain(int argc, wchar_t **argv) {
 			register_distro(new_name, dir);
 			conf.configure_distro(new_name, config_all);
 			copy_directory(get_distro_dir(name), dir);
-		} else if (!wcscmp(argv[1], L"run")) {
+		} else if (!wcscmp(argv[1], L"r") || !wcscmp(argv[1], L"run")) {
 			wstr cmd;
 			bool no_cwd;
 			desc.add_options()
@@ -113,22 +113,22 @@ int wmain(int argc, wchar_t **argv) {
 			auto hr = launch(name.c_str(), cmd.c_str(), !no_cwd, &code);
 			if (FAILED(hr)) throw error_hresult(err_launch_distro, { name }, hr);
 			return code;
-		} else if (!wcscmp(argv[1], L"get-dir")) {
+		} else if (!wcscmp(argv[1], L"gd") || !wcscmp(argv[1], L"get-dir")) {
 			parse_args();
 			std::wcout << get_distro_dir(name);
-		} else if (!wcscmp(argv[1], L"get-env")) {
+		} else if (!wcscmp(argv[1], L"ge") || !wcscmp(argv[1], L"get-env")) {
 			parse_args();
 			reg_config conf;
 			conf.load_distro(name, config_env);
 			for (crwstr s : conf.env) {
 				std::wcout << s << std::endl;
 			}
-		} else if (!wcscmp(argv[1], L"set-env")) {
+		} else if (!wcscmp(argv[1], L"se") || !wcscmp(argv[1], L"set-env")) {
 			reg_config conf;
 			desc.add_options()(",v", po::wvalue<std::vector<wstr>>(&conf.env)->required(), "Environment variables to be set. This argument can be specified multiple times.");
 			parse_args();
 			conf.configure_distro(name, config_env);
-		} else if (!wcscmp(argv[1], L"add-env")) {
+		} else if (!wcscmp(argv[1], L"ae") || !wcscmp(argv[1], L"add-env")) {
 			wstr env;
 			bool force;
 			desc.add_options()
@@ -149,7 +149,7 @@ int wmain(int argc, wchar_t **argv) {
 			}
 			conf.env.push_back(env);
 			conf.configure_distro(name, config_env);
-		} else if (!wcscmp(argv[1], L"remove-env")) {
+		} else if (!wcscmp(argv[1], L"re") || !wcscmp(argv[1], L"remove-env")) {
 			wstr env_name;
 			desc.add_options()(",v", po::wvalue<wstr>(&env_name)->required(), "Name of the environment variable to remove.");
 			parse_args();
@@ -161,51 +161,51 @@ int wmain(int argc, wchar_t **argv) {
 			if (it == conf.env.end()) throw error_other(err_env_not_found, { env_name });
 			conf.env.erase(it);
 			conf.configure_distro(name, config_env);
-		} else if (!wcscmp(argv[1], L"get-uid")) {
+		} else if (!wcscmp(argv[1], L"gu") || !wcscmp(argv[1], L"get-uid")) {
 			parse_args();
 			reg_config conf;
 			conf.load_distro(name, config_uid);
 			std::wcout << conf.uid;
-		} else if (!wcscmp(argv[1], L"set-uid")) {
+		} else if (!wcscmp(argv[1], L"su") || !wcscmp(argv[1], L"set-uid")) {
 			reg_config conf;
 			desc.add_options()(",v", po::wvalue<uint32_t>(&conf.uid)->required(), "UID to be set.");
 			parse_args();
 			conf.configure_distro(name, config_uid);
-		} else if (!wcscmp(argv[1], L"get-kernelcmd")) {
+		} else if (!wcscmp(argv[1], L"gk") || !wcscmp(argv[1], L"get-kernelcmd")) {
 			parse_args();
 			reg_config conf;
 			conf.load_distro(name, config_kernel_cmd);
 			std::wcout << conf.kernel_cmd;
-		} else if (!wcscmp(argv[1], L"set-kernelcmd")) {
+		} else if (!wcscmp(argv[1], L"sk") || !wcscmp(argv[1], L"set-kernelcmd")) {
 			reg_config conf;
 			desc.add_options()(",v", po::wvalue<wstr>(&conf.kernel_cmd)->required(), "Kernel command line to be set.");
 			parse_args();
 			conf.configure_distro(name, config_kernel_cmd);
-		} else if (!wcscmp(argv[1], L"get-flags")) {
+		} else if (!wcscmp(argv[1], L"gf") || !wcscmp(argv[1], L"get-flags")) {
 			parse_args();
 			reg_config conf;
 			conf.load_distro(name, config_flags);
 			std::wcout << conf.flags;
-		} else if (!wcscmp(argv[1], L"set-flags")) {
+		} else if (!wcscmp(argv[1], L"sf") || !wcscmp(argv[1], L"set-flags")) {
 			reg_config conf;
 			desc.add_options()(",v", po::wvalue<uint32_t>(&conf.flags)->required(), "Flags to be set.");
 			parse_args();
 			conf.configure_distro(name, config_flags);
-		} else if (!wcscmp(argv[1], L"shortcut")) {
+		} else if (!wcscmp(argv[1], L"s") || !wcscmp(argv[1], L"shortcut")) {
 			wstr fp, ip;
 			desc.add_options()
 				(",f", po::wvalue<wstr>(&fp)->required(), "Path to the shortcut to be created, including the \".lnk\" suffix.")
 				(",i", po::wvalue<wstr>(&ip), "Path to the icon file for the shortcut. This argument is optional.");
 			parse_args();
 			create_shortcut(name, fp, ip);
-		} else if (!wcscmp(argv[1], L"export-config")) {
+		} else if (!wcscmp(argv[1], L"ec") || !wcscmp(argv[1], L"export-config")) {
 			wstr file;
 			desc.add_options()(",f", po::wvalue<wstr>(&file)->required(), "Path to the XML file to export to.");
 			parse_args();
 			reg_config conf;
 			conf.load_distro(name, config_all);
 			conf.save_file(file);
-		} else if (!wcscmp(argv[1], L"import-config")) {
+		} else if (!wcscmp(argv[1], L"ic") || !wcscmp(argv[1], L"import-config")) {
 			wstr file;
 			desc.add_options()(",f", po::wvalue<wstr>(&file)->required(), "The XML file to import from.");
 			parse_args();
@@ -222,30 +222,30 @@ int wmain(int argc, wchar_t **argv) {
 		} else if (e.msg_code == err_no_action || e.msg_code == err_invalid_action) {
 			std::wcerr
 				<< L"Supported actions are:" << std::endl
-				<< L"    list           List all installed distributions." << std::endl
-				<< L"    get-default    Get the default distribution, which is used by bash.exe." << std::endl
-				<< L"    set-default    Set the default distribution, which is used by bash.exe." << std::endl
-				<< L"    install        Install a new distribution." << std::endl
-				<< L"    uninstall      Uninstall a distribution." << std::endl
-				<< L"    register       Register an existing installation directory." << std::endl
-				<< L"    unregister     Unregister a distribution but not delete the installation directory." << std::endl
-				<< L"    move           Move a distribution to a new directory." << std::endl
-				<< L"    duplicate      Duplicate an existing distribution in a new directory." << std::endl
-				<< L"    run            Run a command in a distribution." << std::endl
-				<< L"    get-dir        Get the installation directory of a distribution." << std::endl
-				<< L"    get-env        Get the default environment variables of a distribution." << std::endl
-				<< L"    set-env        Set the default environment variables of a distribution." << std::endl
-				<< L"    add-env        Add to the default environment variables of a distribution." << std::endl
-				<< L"    remove-env     Remove from the default environment variables of a distribution." << std::endl
-				<< L"    get-uid        Get the UID of the default user of a distribution." << std::endl
-				<< L"    set-uid        Set the UID of the default user of a distribution." << std::endl
-				<< L"    get-kernelcmd  Get the default kernel command line of a distribution." << std::endl
-				<< L"    set-kernelcmd  Set the default kernel command line of a distribution." << std::endl
-				<< L"    get-flags      Get some flags of a distribution. See https://docs.microsoft.com/en-us/previous-versions/windows/desktop/api/wslapi/ne-wslapi-wsl_distribution_flags for details." << std::endl
-				<< L"    set-flags      Set some flags of a distribution. See https://docs.microsoft.com/en-us/previous-versions/windows/desktop/api/wslapi/ne-wslapi-wsl_distribution_flags for details." << std::endl
-				<< L"    shortcut       Create a shortcut to launch a distribution." << std::endl
-				<< L"    export-config  Export configuration of a distribution to an XML file." << std::endl
-				<< L"    import-config  Import configuration of a distribution from an XML file." << std::endl;
+				<< L"    l, list            List all installed distributions." << std::endl
+				<< L"    gd, get-default    Get the default distribution, which is used by bash.exe." << std::endl
+				<< L"    sd, set-default    Set the default distribution, which is used by bash.exe." << std::endl
+				<< L"    i, install         Install a new distribution." << std::endl
+				<< L"    ui, uninstall      Uninstall a distribution." << std::endl
+				<< L"    rg, register        Register an existing installation directory." << std::endl
+				<< L"    ur, unregister     Unregister a distribution but not delete the installation directory." << std::endl
+				<< L"    m, move            Move a distribution to a new directory." << std::endl
+				<< L"    d, duplicate       Duplicate an existing distribution in a new directory." << std::endl
+				<< L"    r, run            Run a command in a distribution." << std::endl
+				<< L"    gd, get-dir        Get the installation directory of a distribution." << std::endl
+				<< L"    ge, get-env        Get the default environment variables of a distribution." << std::endl
+				<< L"    se, set-env        Set the default environment variables of a distribution." << std::endl
+				<< L"    ae, add-env        Add to the default environment variables of a distribution." << std::endl
+				<< L"    re, remove-env     Remove from the default environment variables of a distribution." << std::endl
+				<< L"    gu, get-uid        Get the UID of the default user of a distribution." << std::endl
+				<< L"    su, set-uid        Set the UID of the default user of a distribution." << std::endl
+				<< L"    gk, get-kernelcmd  Get the default kernel command line of a distribution." << std::endl
+				<< L"    sk, set-kernelcmd  Set the default kernel command line of a distribution." << std::endl
+				<< L"    gf, get-flags      Get some flags of a distribution. See https://docs.microsoft.com/en-us/previous-versions/windows/desktop/api/wslapi/ne-wslapi-wsl_distribution_flags for details." << std::endl
+				<< L"    sf, set-flags      Set some flags of a distribution. See https://docs.microsoft.com/en-us/previous-versions/windows/desktop/api/wslapi/ne-wslapi-wsl_distribution_flags for details." << std::endl
+				<< L"    s, shortcut        Create a shortcut to launch a distribution." << std::endl
+				<< L"    ec, export-config  Export configuration of a distribution to an XML file." << std::endl
+				<< L"    ic, import-config  Import configuration of a distribution from an XML file." << std::endl;
 #ifdef LXRUNOFFLINE_VERSION
 			std::wcerr << L"    version        Get version information about this LxRunOffline.exe." << std::endl;
 #endif

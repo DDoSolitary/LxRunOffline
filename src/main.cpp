aafe4a69cd55f104e88ef80ca256b14b1b0ade71
DDoSolitary <DDoSolitary@gmail.com>
2020-06-25 23:39:45 +0800

    Use raw string literal to write help message.

--- src/main.cpp
+++ src/main.cpp
@@ -297,36 +297,36 @@ int wmain(int argc, wchar_t **argv) {
 	} catch (const lro_error &e) {
 		log_error(e.format());
 		if (e.msg_code == err_msg::err_no_action || e.msg_code == err_msg::err_invalid_action) {
-			std::wcerr
-				<< L"Supported actions are:\n"
-				<< L"    l, list            List all installed distributions.\n"
-				<< L"    gd, get-default    Get the default distribution, which is used by bash.exe.\n"
-				<< L"    sd, set-default    Set the default distribution, which is used by bash.exe.\n"
-				<< L"    i, install         Install a new distribution.\n"
-				<< L"    ui, uninstall      Uninstall a distribution.\n"
-				<< L"    rg, register       Register an existing installation directory.\n"
-				<< L"    ur, unregister     Unregister a distribution but not delete the installation directory.\n"
-				<< L"    m, move            Move a distribution to a new directory.\n"
-				<< L"    d, duplicate       Duplicate an existing distribution in a new directory.\n"
-				<< L"    e, export          Export a distribution's filesystem to a .tar.gz file, which can be imported by the \"install\" command.\n"
-				<< L"    r, run             Run a command in a distribution.\n"
-				<< L"    di, get-dir        Get the installation directory of a distribution.\n"
-				<< L"    gv, get-version    Get the filesystem version of a distribution.\n"
-				<< L"    ge, get-env        Get the default environment variables of a distribution.\n"
-				<< L"    se, set-env        Set the default environment variables of a distribution.\n"
-				<< L"    ae, add-env        Add to the default environment variables of a distribution.\n"
-				<< L"    re, remove-env     Remove from the default environment variables of a distribution.\n"
-				<< L"    gu, get-uid        Get the UID of the default user of a distribution.\n"
-				<< L"    su, set-uid        Set the UID of the default user of a distribution.\n"
-				<< L"    gk, get-kernelcmd  Get the default kernel command line of a distribution.\n"
-				<< L"    sk, set-kernelcmd  Set the default kernel command line of a distribution.\n"
-				<< L"    gf, get-flags      Get some flags of a distribution. See https://docs.microsoft.com/en-us/previous-versions/windows/desktop/api/wslapi/ne-wslapi-wsl_distribution_flags for details.\n"
-				<< L"    sf, set-flags      Set some flags of a distribution. See https://docs.microsoft.com/en-us/previous-versions/windows/desktop/api/wslapi/ne-wslapi-wsl_distribution_flags for details.\n"
-				<< L"    s, shortcut        Create a shortcut to launch a distribution.\n"
-				<< L"    ec, export-config  Export configuration of a distribution to an XML file.\n"
-				<< L"    ic, import-config  Import configuration of a distribution from an XML file.\n"
-				<< L"    sm, summary        Get general information of a distribution.\n"
-				<< L"    version            Get version information about this LxRunOffline.exe.\n";
+			std::wcerr << R"(Supported actions are:
+    l, list            List all installed distributions.
+    gd, get-default    Get the default distribution, which is used by bash.exe.
+    sd, set-default    Set the default distribution, which is used by bash.exe.
+    i, install         Install a new distribution.
+    ui, uninstall      Uninstall a distribution.
+    rg, register       Register an existing installation directory.
+    ur, unregister     Unregister a distribution but not delete the installation directory.
+    m, move            Move a distribution to a new directory.
+    d, duplicate       Duplicate an existing distribution in a new directory.
+    e, export          Export a distribution's filesystem to a .tar.gz file, which can be imported by the "install" command.
+    r, run             Run a command in a distribution.
+    di, get-dir        Get the installation directory of a distribution.
+    gv, get-version    Get the filesystem version of a distribution.
+    ge, get-env        Get the default environment variables of a distribution.
+    se, set-env        Set the default environment variables of a distribution.
+    ae, add-env        Add to the default environment variables of a distribution.
+    re, remove-env     Remove from the default environment variables of a distribution.
+    gu, get-uid        Get the UID of the default user of a distribution.
+    su, set-uid        Set the UID of the default user of a distribution.
+    gk, get-kernelcmd  Get the default kernel command line of a distribution.
+    sk, set-kernelcmd  Set the default kernel command line of a distribution.
+    gf, get-flags      Get some flags of a distribution. See https://docs.microsoft.com/en-us/previous-versions/windows/desktop/api/wslapi/ne-wslapi-wsl_distribution_flags for details.
+    sf, set-flags      Set some flags of a distribution. See https://docs.microsoft.com/en-us/previous-versions/windows/desktop/api/wslapi/ne-wslapi-wsl_distribution_flags for details.
+    s, shortcut        Create a shortcut to launch a distribution.
+    ec, export-config  Export configuration of a distribution to an XML file.
+    ic, import-config  Import configuration of a distribution from an XML file.
+    sm, summary        Get general information of a distribution.
+    version            Get version information about this LxRunOffline.exe.
+)";
 		}
 		return 1;
 	} catch (const po::error &e) {
